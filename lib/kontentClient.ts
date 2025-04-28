6857bd3083f6bc5ead6ee31734d7561bc0a5cb35
Joshua Seprodi <joshua.seprodi@kontent.ai>
2025-04-28 10:08:56 -0400

    fixed variable

--- lib/kontentClient.ts
+++ lib/kontentClient.ts
@@ -3,8 +3,8 @@ import { CoreClientTypes } from "./models/system/core.type.js";
 import { NavigationItemRoot, isNavigationItemRoot } from "./models/content-types/web_spotlight_root";
 import { Band } from "./models/content-types/band"; 
 import { IContentItem } from "@kontent-ai/delivery-sdk";
-
-const environmentId = c1ad4901-f748-000b-2b83-2c4fa51e2983;
+c1ad4901-f748-000b-2b83-2c4fa51e2983
+const environmentId = NEXT_PUBLIC_KONTENT_ENVIRONMENT_ID;
 if (!environmentId) {
   throw new Error("NEXT_PUBLIC_KONTENT_ENVIRONMENT_ID environment variable is not set.");
 }
